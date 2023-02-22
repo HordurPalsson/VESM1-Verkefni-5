2d8145ad623df3b67558c53a6db7e4612e166484
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-22 14:06:41 +0000

    Update

--- Efni/Kóði/skeleton_Trigger/skeleton_Trigger.ino
+++ Efni/Kóði/skeleton_Trigger/skeleton_Trigger.ino
@@ -1,9 +1,15 @@
 // Include NewPing Library
 #include "NewPing.h"
 #include "Servo.h"
+#include "tdelay.h"
 
 // Servo Object
 Servo myservo;
+
+Servo motor;
+int motorPin = 12;
+int stefna = 0;
+bool movement = true;
 
 // Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
 #define TRIGGER_PIN 9
@@ -24,22 +30,34 @@ void setup() {
   Serial.begin(9600);
   // Kveikir servoinum
   myservo.attach(servoPin);
+  // motorinn
+  motor.attach(motorPin);
+  motor.write(stefna);
 }
 
 void loop() {
-  if (sonar.ping_cm() > 20) {
-    Serial.print("OFF");
-    //Controls Skeleton
-    //ArduinoFFT could automate this
-    myservo.write(120);
-    delay(300);
-    myservo.write(30);
-    delay(150);
-    myservo.write(0);
-    delay(250);
-  } else {
-    Serial.print("ON");
-    myservo.write(0);
+    if (sonar.ping_cm() > 10) {
+      if (movement == true) {
+        stefna++;  // jafngildir stefna += 1 og stefna = stefna + 1
+      } else {     // mótorinn er að hreyfast til hægri
+        stefna--;  // jafngildir stefna -= 1 og stefna = stefna - 1
+      }
+      if (stefna == 0 || stefna == 180) {  // ef mótirnn er kominn út á enda, víxla áttunum
+        movement = !movement;
+      }
+      motor.write(stefna);
+
+    if (sonar.ping_cm() > 20) {
+      Serial.print("OFF");
+      //Controls Skeleton
+      //ArduinoFFT potentionally could automate this
+      myservo.write(120);
+      myservo.write(30);
+      myservo.write(0);
+    } else {
+      Serial.print("ON");
+      myservo.write(0);
+    }
+    delay(625);
   }
-  delay(625);
 }
