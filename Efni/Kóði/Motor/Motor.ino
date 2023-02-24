5ba45c98c481f20b5fae24292cd7f5fc5e1409cb
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-24 09:59:53 +0000

    Update

--- Efni/Kóði/Motor/Motor.ino
+++ Efni/Kóði/Motor/Motor.ino
@@ -1,11 +1,11 @@
-#include <Servo.h> // Sambærilegt og import í python
+#include <Servo.h>
 #include "tdelay.h"
 
 Servo motor; // bý til tilvik af Servo klasanum
 int motor_pinni = 9; // pinninn sem ég nota til að stjórna mótornum
 
 // svipað og listi í python, geymir stefnurnar sem mótorinn á að fara í og í hvaða röð
-int motor_stefnur[] = {0, 90, 180, 90}; 
+int motor_stefnur[] = {0, 45, 90, 135}; 
 int motor_stefnu_fjoldi = 4; // breytan geymir hversu margar stefnur eru í listanum
 int motor_stefnu_teljari = 0; // breytan heldur utan um í hvaða stefnu mótorinn á að benda
 
