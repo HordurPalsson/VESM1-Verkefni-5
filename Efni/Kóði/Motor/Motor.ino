e91b001321b67af47146acc9f8b000b074c261c1
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-24 10:02:46 +0000

    Update

--- Efni/Kóði/Motor/Motor.ino
+++ Efni/Kóði/Motor/Motor.ino
@@ -5,8 +5,7 @@
 Servo motor; // bý til tilvik af Servo klasanum
 int motor_pinni = 9; // pinninn sem ég nota til að stjórna mótornum
 
-// svipað og listi í python, geymir stefnurnar sem mótorinn á að fara í og í hvaða röð
-int motor_stefnur[] = {0, 45, 90, 135}; 
+int motor_stefnur[] = {0, 45, 90, 135}; // 360 movement
 int motor_stefnu_fjoldi = 4; // breytan geymir hversu margar stefnur eru í listanum
 int motor_stefnu_teljari = 0; // breytan heldur utan um í hvaða stefnu mótorinn á að benda
 
