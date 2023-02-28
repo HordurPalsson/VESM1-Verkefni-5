df962e5c1d93baef5b0b6a0a6e326c7641432905
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-28 09:46:47 +0000

    Update

--- Efni/Kóði/code_Samsettning/code_Samsettning.ino
+++ Efni/Kóði/code_Samsettning/code_Samsettning.ino
@@ -4,7 +4,8 @@
 #include <Servo.h>
 
 // Objects
-// Tölur eru hvaða pinnar fara á arduinoinn
+// Tölur eru hvaða pinnar fara á arduino-inn
+// Skynjari
 #define TRIGGER_PIN 9  // Fyrir skynjarann
 #define ECHO_PIN 10    // Fyrir skynjarann
 
@@ -28,7 +29,6 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 TDelay motor_bid(500);  // bíða í hálfa sekúndu á milli hreyfinga
 
-
 void setup() {
   Serial.begin(9600);
   motor.attach(motor_pinni);                         // segir servo tilvikinu hvaða pinna á að nota
@@ -36,14 +36,16 @@ void setup() {
 }
 
 void loop() {
+  if (sonar.ping_cm()
+  
   if (sonar.ping_cm() < distance) {
-    if(motor_bid.timiLidinn()) {
-    // uppfæra stefnu_teljara breytuna, modulus notað til að talan verði
-    // aldrei hærri en fjöldi stefnanna sem eru í listanum
-    motor_stefnu_teljari = (motor_stefnu_teljari + 1) % motor_stefnu_fjoldi;
-    // veljum svo rétta stefnu úr listanum
-    motor.write(motor_stefnur[motor_stefnu_teljari]);
-  }
+    if (motor_bid.timiLidinn()) {
+      // uppfæra stefnu_teljara breytuna, modulus notað til að talan verði
+      // aldrei hærri en fjöldi stefnanna sem eru í listanum
+      motor_stefnu_teljari = (motor_stefnu_teljari + 1) % motor_stefnu_fjoldi;
+      // veljum svo rétta stefnu úr listanum
+      motor.write(motor_stefnur[motor_stefnu_teljari]);
+    }
 
     // Keyrir í staðinn ef enginn er nálagt
   } else {
