ba5a9b2c34b2a6f28408dd4b21c76c5516fbbc45
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-28 09:26:17 +0000

    Update

--- Efni/Kóði/code_Samsettning/code_Samsettning.ino
+++ Efni/Kóði/code_Samsettning/code_Samsettning.ino
@@ -7,12 +7,14 @@
 #define TRIGGER_PIN 9
 #define ECHO_PIN 10
 
+
+// Hversu nálagt þarf að vera til að
+// kveikja á kóðanum í cm
 int distance = 20;
 
 // Maximum distance we want to ping for (in centimeters).
 #define MAX_DISTANCE 400
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 
 void setup() {
@@ -21,7 +23,9 @@ void setup() {
 
 void loop() {
   if (sonar.ping_cm() < distance) {
-      
+    
+
+    // Keyrir í staðinn ef enginn er nálagt
     } else {
     
     }
