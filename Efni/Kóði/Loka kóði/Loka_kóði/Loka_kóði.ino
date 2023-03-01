17e433d2410e392138ed7c730c1ebe1d516a960e
Árni Hrafn <arnihrafn05@gmail.com>
2023-03-01 12:31:38 +0000

    Create Loka_kóði.ino

---
+++ Efni/Kóði/Loka kóði/Loka_kóði/Loka_kóði.ino
@@ -0,0 +1,41 @@
+// Libraries
+#include "NewPing.h"
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+#include "tdelay.h"
+#include <Servo.h>
+
+// DC mótor
+const int HRADI = 5;  // Verður að vera PWM pinni
+const int STEFNA_A = 2;
+const int STEFNA_B = 4;
+TDelay DC_Motorbid(1000) //Bíður í eina sek
+void afram(int hradi);
+TDelay DC_Motorbid(2000) //Bíður í eina sek
+void bakka(int hradi);
+void stoppa();
+
+//
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// DC mótor
+void afram(int hradi) {
+  if(DC_Motorbid.timilidinn())
+    digitalWrite(STEFNA_A, HIGH);
+    digitalWrite(STEFNA_B, LOW);
+    analogWrite(HRADI, hradi);
+}
+
+void stoppa() {
+    digitalWrite(STEFNA_A, LOW);
+    digitalWrite(STEFNA_B, LOW);
+    analogWrite(HRADI, 0);
+}
