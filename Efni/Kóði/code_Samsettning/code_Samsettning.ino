4c93ebe39d6539ca33415d4845b95d21dffa83cd
HordurPalsson <hordur.palsson9@gmail.com>
2023-03-01 11:53:28 +0000

    Update code_Samsettning.ino

--- Efni/Kóði/code_Samsettning/code_Samsettning.ino
+++ Efni/Kóði/code_Samsettning/code_Samsettning.ino
@@ -6,75 +6,108 @@
 #include "tdelay.h"
 #include <Servo.h>
 
-// Objects
-// Tölur eru hvaða pinnar fara á arduino-inn
-// Skynjari
-#define TRIGGER_PIN 9  // Fyrir skynjarann
-#define ECHO_PIN 10    // Fyrir skynjarann
+// Servo
+Servo motor;                
+int motor_pinni = 4;                      // pinninn sem ég nota til að stjórna mótornum
+int motor_stefnur[] = {0, 90, 180, 90};   // geymir stefnurnar sem mótorinn á að fara í og í hvaða röð
+int motor_stefnu_fjoldi = 4;              // breytan geymir hversu margar stefnur eru í listanum
+int motor_stefnu_teljari = 0;             // breytan heldur utan um í hvaða stefnu mótorinn á að benda
+void munnur();                            // fall útfært neðar
+TDelay motor_bid(500);                    // bíða í hálfa sekúndu á milli hreyfinga
 
 // DC mótor
-const int HRADI = 5;  // Verður að vera PWM pinni
+const int HRADI = 5;                      // Verður að vera PWM pinni
 const int STEFNA_A = 2;
 const int STEFNA_B = 4;
 void afram(int hradi);
 void bakka(int hradi);
 void stoppa();
 
-Servo motor;
-int motor_pinni = 11;
+/*
+  VCC tengist í 5V
+  Trig tengist í digital pinna. Þarf EKKI að vera PWM.
+  Echo tegist í digital pinna. Þarf EKKI að vera PWM.
+  Gnd tengist í GND.
+*/
 
-// Motor
-int motor_stefnur[] = { 0, 45, 90, 135 };  // 360 movement
-int motor_stefnu_fjoldi = 4;               // breytan geymir hversu margar stefnur eru í listanum
-int motor_stefnu_teljari = 0;              // breytan heldur utan um í hvaða stefnu mótorinn á að benda
-
-// Hversu nálagt þarf að vera til að
-// kveikja á kóðanum í cm
-int distance = 20;
-
-// Maximum distance we want to ping for (in centimeters).
-#define MAX_DISTANCE 400
-
-// NewPing setup of pins and maximum distance.
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-
-TDelay motor_bid(500);  // bíða í hálfa sekúndu á milli hreyfinga
+// Skynjari
+const int ECHO = 2; // digital pinni
+const int TRIG = 3; // digital pinni
+int distance = 0;  
+int fjarlaegd();  // fall sem sér um fjarlægðamælinguna, skilar fjarlægð í cm. 
 
 void setup() {
-  Serial.begin(9600);
-  motor.attach(motor_pinni);                         // segir servo tilvikinu hvaða pinna á að nota
-  motor.write(motor_stefnur[motor_stefnu_teljari]);  // í þessu tilfelli á mótorinn að byrja í 0°
-  stoppa();                                          // upphafsstaðan, verður stopp þar til annað er ákveðið
+
+  // Skynjari
+  Serial.begin(9600); 
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
+
+  // Servo
+  motor.attach(motor_pinni);    // segi servo tilvikinu hvaða pinna á að nota
+  motor.write(0);               // mótorinn að byrja í 0°
+
+  pinMode(HRADI, OUTPUT);
+  pinMode(STEFNA_A, OUTPUT);
+  pinMode(STEFNA_B, OUTPUT);
+  stoppa();                    // upphafsstaðan, verður stopp þar til annað er ákveðið 
+  
 }
 
-void loop() {
-  if (sonar.ping_cm() < 0) {
+void talk() {
+  afram(150);
+  munnur();
+}
+
+void loop () {
     
-  }
+    distance = fjarlaegd();         // fáum fjarlægðamælingu í cm.
+  
+    // fjarlægð getur ekki verið mínustala
+    if(distance < 0) { 
+      distance = 0;
+    } 
+  
+    // ef fjarlægð í hlut er minna en 50 cm, má ekki vera neikvæð tala.
+    if(distance < 50 && distance != 0) {  
+      // setja sýninguna af stað
 
-  if (sonar.ping_cm() < distance) {
-    if (motor_bid.timiLidinn()) {
-      // uppfæra stefnu_teljara breytuna, modulus notað til að talan verði
-      // aldrei hærri en fjöldi stefnanna sem eru í listanum
-      motor_stefnu_teljari = (motor_stefnu_teljari + 1) % motor_stefnu_fjoldi;
-      // veljum svo rétta stefnu úr listanum
-      motor.write(motor_stefnur[motor_stefnu_teljari]);
-    }
 
-    // Keyrir í staðinn ef enginn er nálagt
-  } else {
-  }
+      if(motor_bid.timiLidinn()) {
+        // uppfæra stefnu_teljara breytuna, modulus notað til að talan verði
+        // aldrei hærri en fjöldi stefnanna sem eru í listanum
+        motor_stefnu_teljari = (motor_stefnu_teljari + 1) % motor_stefnu_fjoldi;
+        // veljum svo rétta stefnu úr listanum
+        motor.write(motor_stefnur[motor_stefnu_teljari]);
+      }
+      
+    } 
+   
 }
 
 // DC mótor
 void afram(int hradi) {
-  digitalWrite(STEFNA_A, HIGH);
-  digitalWrite(STEFNA_B, LOW);
-  analogWrite(HRADI, hradi);
+    digitalWrite(STEFNA_A, HIGH);
+    digitalWrite(STEFNA_B, LOW);
+    analogWrite(HRADI, hradi);
 }
 
 void stoppa() {
-  digitalWrite(STEFNA_A, LOW);
-  digitalWrite(STEFNA_B, LOW);
-  analogWrite(HRADI, 0);
+    digitalWrite(STEFNA_A, LOW);
+    digitalWrite(STEFNA_B, LOW);
+    analogWrite(HRADI, 0);
+}
+
+int fjarlaegd() {
+  
+    // sendir út púlsa
+    digitalWrite(TRIG,LOW);
+    delayMicroseconds(2); // of stutt delay til að skipta máli
+    digitalWrite(TRIG,HIGH);
+    delayMicroseconds(10); // of stutt delay til að skipta máli
+    digitalWrite(TRIG,LOW);
+
+    // mælt hvað púlsarnir voru lengi að berast til baka
+    return (int)pulseIn(ECHO,HIGH)/58.0; // deilt með 58 til að breyta í cm
+  
 }
