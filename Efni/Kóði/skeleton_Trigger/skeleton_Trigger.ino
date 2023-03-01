b392704ef077ca6d36e53b8cef02f11d483d4b55
HordurPalsson <hordur.palsson9@gmail.com>
2023-03-01 13:03:47 +0000

    Update skeleton_Trigger.ino

--- Efni/Kóði/skeleton_Trigger/skeleton_Trigger.ino
+++ Efni/Kóði/skeleton_Trigger/skeleton_Trigger.ino
@@ -1,27 +1,51 @@
-// Include NewPing Library
-#include "NewPing.h"
+/*
+  VCC tengist í 5V
+  Trig tengist í digital pinna. Þarf EKKI að vera PWM.
+  Echo tegist í digital pinna. Þarf EKKI að vera PWM.
+  Gnd tengist í GND.
+*/
 
-// Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
-#define TRIGGER_PIN 9
-#define ECHO_PIN 10
-
-// Maximum distance we want to ping for (in centimeters).
-#define MAX_DISTANCE 400
-
-// NewPing setup of pins and maximum distance.
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+const int ECHO = 2; 
+const int TRIG = 3; 
+int distance = 0;  
+int fjarlaegd();  // fall sem sér um fjarlægðamælinguna, skilar fjarlægð í cm. 
 
 void setup() {
-	Serial.begin(9600);
+  
+    Serial.begin(9600); 
+    pinMode(TRIG,OUTPUT);
+    pinMode(ECHO,INPUT);
+  
 }
 
-void loop() {
-	// Serial.print(sonar.ping_cm());
-	// Serial.println(" cm");
-  if (sonar.ping_cm() > 100)  {
-    Serial.print("OFF = ");
-  } else {
-    Serial.print("ON = ");
-  }
-  delay(625);
+void loop () {
+    
+    distance = fjarlaegd()           // fáum fjarlægðamælingu í cm.
+    Serial.print("Fjarlaegd: ");     // til að sjá mælingu í skjánum
+    Serial.println(fjarlaegd());     // til að sjá mælingu í skjánum
+  
+    // fjarlægð getur ekki verið mínustala
+    if(distance < 0) { 
+      distance = 0;
+    } 
+  
+    // ef fjarlægð í hlut er minna en 50 cm, má ekki vera neikvæð tala.
+    if(distance < 50 && distance != 0) {  
+      // setja sýninguna af stað
+    } 
+   
+}
+
+int fjarlaegd() {
+  
+    // sendir út púlsa
+    digitalWrite(TRIG,LOW);
+    delayMicroseconds(2); // of stutt delay til að skipta máli
+    digitalWrite(TRIG,HIGH);
+    delayMicroseconds(10); // of stutt delay til að skipta máli
+    digitalWrite(TRIG,LOW);
+
+    // mælt hvað púlsarnir voru lengi að berast til baka
+    return (int)pulseIn(ECHO,HIGH)/58.0; // deilt með 58 til að breyta í cm
+  
 }
